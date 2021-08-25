0aca509e8cc3da0ac1312050dcbe92f87dcfc970
ggg05158 <csg05158@naver.com>
2021-08-25 15:51:16 +0100

    8-25study

--- 21-8-25/basicTest2.js
+++ 21-8-25/basicTest2.js
@@ -9,6 +9,3 @@ const candyMachine = {
     }
 };
 const {getCandy, status: {count}} = candyMachine;
-console.log(candyMachine.status.count); //현재는 5
-console.log(getCandy()); //count를 1감소
-console.log(candyMachine.status.count); //값은 4
